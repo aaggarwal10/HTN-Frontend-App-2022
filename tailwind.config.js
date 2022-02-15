27d192aac627f423a97dc2e2fb3121376b6786c3
aaggarwal10 <anish.5@hotmail.com>
2022-02-14 21:34:14 -0500

    Added TailwindCSS & Toggle Dark Mode

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './pages/**/*.{js,ts,tsx,jsx}',
+    './components/**/*.{js,ts,tsx,jsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
